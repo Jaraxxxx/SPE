842c077b5395c99f14614325f993c949fb660540
Jay Rathod <49514423+Jaraxxxx@users.noreply.github.com>
2024-02-17 01:14:54 +0530

    Update main.cpp

--- MiniProject/main.cpp
+++ MiniProject/main.cpp
@@ -1,41 +1,49 @@
+#include <gtest/gtest.h>
 #include "libs/calc/Calculator.h"
+#include "libs/Calculator.cpp"
+
 
 int main(){
     Calculator c;
-    int choice;
-    cout<<"######### Welcome to Calculator #########"<<endl;
-    cout<<"Enter Option :\n\t1. Factorial\n\t2. Square Root\n\t3. Natural Logarithm\n\t4. Power\n\t5. Exit";
-    cin>>choice;
-    while(choice != 5){
-        switch(choice){
+    int choice = 0,n = 0,base = 0, pwr = 0;
+    do{
+        cout<<"######### Welcome to Calculator #########"<<endl;
+        cout<<"Enter Option :\n\t1. Factorial\n\t2. Square Root\n\t3. Natural Logarithm\n\t4. Power\n\t5. Exit\n";
+        cin>>choice;
+
+        int n, base, pwr; // Declare variables outside the switch block
+
+        switch(choice) {
             case 1:
-                int n = 0;
                 cout<<"######### Selected Factorial Function #########\nEnter number :\n";
                 cin>>n;
                 cout<<c.factorial(n)<<endl;
                 break;
             case 2:
-                int n = 0;
                 cout<<"######### Selected Square root Function #########\nEnter number :\n";
                 cin>>n;
                 cout<<c.root(n)<<endl;
                 break;
             case 3:
-                int n = 0;
                 cout<<"######### Selected Natural Logarithmic Function #########\nEnter number :\n";
                 cin>>n;
                 cout<<c.natlog(n)<<endl;
                 break;
             case 4:
-                int base = 0,pwr = 0;
-                cout<<"######### Selected Power Function #########\nEnter base :\nEnter power :";
-                cin>>base>>pwr;
+                cout<<"######### Selected Power Function #########\nEnter base : \n";
+                cin >> base;
+                cout << "Enter power : \n";
+                cin >> pwr;
                 cout<<c.power(base,pwr)<<endl;
                 break;
             case 5:
                 cout<<"######### Thank You for using the app #########\n";
                 break;
-        }
-    }
+            default: 
+                cout<<"Invalid choice selected\n";
+                break;
+        }   
+
+    } while (choice != 5);
     return 0;
 }
