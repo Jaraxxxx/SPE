16a9cc8b8ca91370d556f8eb6c801dea4e4ae819
Jay Rathod <49514423+Jaraxxxx@users.noreply.github.com>
2024-02-16 18:52:49 +0000

    Calculator v1
    Signed-off-by: Jay Rathod <49514423+Jaraxxxx@users.noreply.github.com>

--- MiniProject/main.cpp
+++ MiniProject/main.cpp
@@ -1,10 +1,41 @@
 #include "libs/calc/Calculator.h"
-#
+
 int main(){
     Calculator c;
-    cout<<c.factorial(10)<<endl;
-    cout<<c.root(100)<<endl;
-    cout<<c.natlog(1)<<endl;
-    cout<<c.power(10,10)<<endl;
+    int choice;
+    cout<<"######### Welcome to Calculator #########"<<endl;
+    cout<<"Enter Option :\n\t1. Factorial\n\t2. Square Root\n\t3. Natural Logarithm\n\t4. Power\n\t5. Exit";
+    cin>>choice;
+    while(choice != 5){
+        switch(choice){
+            case 1:
+                int n = 0;
+                cout<<"######### Selected Factorial Function #########\nEnter number :\n";
+                cin>>n;
+                cout<<c.factorial(n)<<endl;
+                break;
+            case 2:
+                int n = 0;
+                cout<<"######### Selected Square root Function #########\nEnter number :\n";
+                cin>>n;
+                cout<<c.root(n)<<endl;
+                break;
+            case 3:
+                int n = 0;
+                cout<<"######### Selected Natural Logarithmic Function #########\nEnter number :\n";
+                cin>>n;
+                cout<<c.natlog(n)<<endl;
+                break;
+            case 4:
+                int base = 0,pwr = 0;
+                cout<<"######### Selected Power Function #########\nEnter base :\nEnter power :";
+                cin>>base>>pwr;
+                cout<<c.power(base,pwr)<<endl;
+                break;
+            case 5:
+                cout<<"######### Thank You for using the app #########\n";
+                break;
+        }
+    }
     return 0;
 }
