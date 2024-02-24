b2cdc7a205751e223cb701b4697fd0cf1d519be6
Jay <jay.m.rathod.01@gmail.com>
2024-02-24 12:11:37 +0530

    Temp test
    Signed-off-by: Jay <jay.m.rathod.01@gmail.com>

--- MiniProject/build/Calculator_TEST[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "C:/Users/jaymr/MegaSync/DATABASE/XJay Docs/IIIT_Bangalore/MTech/Term2/SPE/MSVC/Calculator/MiniProject/build/Calculator_TEST[1]_tests.cmake")
-  include("C:/Users/jaymr/MegaSync/DATABASE/XJay Docs/IIIT_Bangalore/MTech/Term2/SPE/MSVC/Calculator/MiniProject/build/Calculator_TEST[1]_tests.cmake")
-else()
-  add_test(Calculator_TEST_NOT_BUILT Calculator_TEST_NOT_BUILT)
-endif()
--- MiniProject/out/build/x64-Debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/jaymr/MegaSync/DATABASE/XJay Docs/IIIT_Bangalore/MTech/Term2/SPE/MSVC/Calculator/MiniProject
-# Build directory: C:/Users/jaymr/MegaSync/DATABASE/XJay Docs/IIIT_Bangalore/MTech/Term2/SPE/MSVC/Calculator/MiniProject/out/build/x64-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Users/jaymr/MegaSync/DATABASE/XJay Docs/IIIT_Bangalore/MTech/Term2/SPE/MSVC/Calculator/MiniProject/out/build/x64-Debug/Calculator_test[1]_include.cmake")
-subdirs("_deps/googletest-build")
