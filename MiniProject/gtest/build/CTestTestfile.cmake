2ee3a5c939c18939602966c19f5c76333d5fe24e
Jay Rathod <49514423+Jaraxxxx@users.noreply.github.com>
2024-02-27 16:44:04 +0000

    deleting redundant files

--- MiniProject/gtest/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/jaymr/MegaSync/DATABASE/XJay Docs/IIIT_Bangalore/MTech/Term2/SPE/MiniProject/gtest
-# Build directory: C:/Users/jaymr/MegaSync/DATABASE/XJay Docs/IIIT_Bangalore/MTech/Term2/SPE/MiniProject/gtest/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Users/jaymr/MegaSync/DATABASE/XJay Docs/IIIT_Bangalore/MTech/Term2/SPE/MiniProject/gtest/build/hello_test[1]_include.cmake")
-subdirs("_deps/googletest-build")
--- MiniProject/gtest/build/hello_test[1]_tests.cmake
+++
@@ -1,3 +0,0 @@
-add_test([=[HelloTest.BasicAssertions]=]  [==[C:/Users/jaymr/MegaSync/DATABASE/XJay Docs/IIIT_Bangalore/MTech/Term2/SPE/MiniProject/gtest/build/Debug/hello_test.exe]==] [==[--gtest_filter=HelloTest.BasicAssertions]==] --gtest_also_run_disabled_tests)
-set_tests_properties([=[HelloTest.BasicAssertions]=]  PROPERTIES WORKING_DIRECTORY [==[C:/Users/jaymr/MegaSync/DATABASE/XJay Docs/IIIT_Bangalore/MTech/Term2/SPE/MiniProject/gtest/build]==] SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-set(  hello_test_TESTS HelloTest.BasicAssertions)
